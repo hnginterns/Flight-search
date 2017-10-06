a8a9c19fc35afac7374bca05ea9abbac2419f765
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-06 21:30:50 +0100

    changed to md5 hashing

--- database/factories/Api_keyFactory.php
+++ database/factories/Api_keyFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Api_key::class, function (Faker $faker) {
     return [
-        'api_key'=>bcrypt(rand(0, 1000))
+        'api_key'=>md5(rand(0, 1000))
     ];
 });
